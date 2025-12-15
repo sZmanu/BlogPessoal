ad64396a13ee77fbbf5e9aa12d4a5a7ab815cb76
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2025-12-15 14:10:17 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,24 +8,18 @@ import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 // arquivos module, serve para configuração
-
-//decorator, uma etiqueta de metadados, indica o que é a classe é um module
-// os decorator muda o comportamento da classe, e trás uma importancia maior
 @Module({
 imports: [  // Configurando o TypeORM
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'admin',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	  useClass: ProdService,
+    imports: [ConfigModule],
+}),
     PostagemModule,
     TemaModule,
     AuthModule,
